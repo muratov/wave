f988a979c67163f785587b9fc4610013ba1f0e3c
Michail Muratov <warlock2k@gmail.com>
2011-08-03 00:19:17 +0400

    первая выдача

--- index.php
+++ index.php
@@ -3,11 +3,18 @@
 	require_once 'yandex.php';
 	require_once 'parser.class.php';
 
+	isset($_GET['q']) ? $test = new yandex($_GET['q'],2,5):$test = new yandex("jquery habrahabr",2,5);
 
-	$test = new yandex("hello world",2,5);
 	echo $test->getParseURL();
+	echo "<br/><br/>";
 	$test2 = new SEParser($test->getParseURL());
-	echo $test2->title;
+
+	//print_r ($test2->titles);
+	$i=0;
+	foreach ($test2->titles as $title) {
+		echo '<a href='.$test2->urls[$i++].'>'.$title.'</a><br/>';
+	}
+	//print_r ($test2->urls);
 
 
 ?>
--- parser.class.php
+++ parser.class.php
@@ -1,15 +1,17 @@
 <?php
 include_once 'phpQuery-onefile.php';
 	class SEParser {
-		var $title;
-		var $titleElement;
+		var $titles = array();
+		var $urls = array();
+
 
 		public function __construct($url) {
-
 			phpQuery::newDocumentFileHTML($url);
-			$this->title = pq('title')->html();
-			//$this->title = $this->titleElement;
-			$this->title;
+			$results = pq('.b-serp-item');
+			foreach ($results as $li) {
+				array_push($this->titles, pq($li)->find('.b-serp-item__title-link')->text());
+				array_push($this->urls, pq($li)->find("a")->attr("href"));
+			}
 
 		}
 
